0662ebef182c90dba030a8f33be969fbcbc8a7f6
unisexx <unisexx@hotmail.com>
2013-05-25 10:28:09 +0700

    ...

--- themes/admin/layout.php
+++ themes/admin/layout.php
@@ -6,23 +6,72 @@
 		<title><?php echo $template['title']; ?></title> 
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 		<?php include_once '_css.php'; ?>
-		<?php include_once '_script.php'; ?>
 	</head>
 	<body>
-        <?php include_once '_header.inc.php'; ?>
-        <div class="container-fluid">
-          <div class="row-fluid">
-            <div id="menu" class="span3">
-              <div class="sidebar-nav bs-docs-sidebar">
-                  <?php include_once '_menu.inc.php'; ?>
-              </div><!--/.well -->
-            </div><!--/span-->
+        <?php include_once '_header.php'; ?>
+
+        <div class="container-fluid" id="main-container">
+            <a href="#" id="menu-toggler"><span></span></a><!-- menu toggler -->
             
-            <div class="span9">
-              <div class="row-fluid" style="padding-top:10px;">
-                <?php echo $template['body']; ?>
-              </div><!--/row-->
-            </div><!--/span-->
-          </div><!--/row-->
+            <?php include_once '_menu.php'?>
+            <div id="main-content" class="clearfix">
+                    
+                    <div id="breadcrumbs">
+                        <ul class="breadcrumb">
+                            <li><i class="icon-home"></i> <a href="#">Home</a><span class="divider"><i class="icon-angle-right"></i></span></li>
+                            <li><a href="#">Other Pages</a> <span class="divider"><i class="icon-angle-right"></i></span></li>
+                            <li class="active">Blank Page</li>
+                        </ul><!--.breadcrumb-->
+
+                        <div id="nav-search">
+                            <form class="form-search">
+                                    <span class="input-icon">
+                                        <input autocomplete="off" id="nav-search-input" type="text" class="input-small search-query" placeholder="Search ..." />
+                                        <i id="nav-search-icon" class="icon-search"></i>
+                                    </span>
+                            </form>
+                        </div><!--#nav-search-->
+                    </div><!--#breadcrumbs-->
+
+
+
+                    <div id="page-content" class="clearfix">
+                        <div class="row-fluid">
+<!-- PAGE CONTENT BEGINS HERE -->
+<!-- PAGE CONTENT ENDS HERE -->
+                         </div><!--/row-->
+                    </div><!--/#page-content-->
+                      
+
+                    <div id="ace-settings-container">
+                        <div class="btn btn-app btn-mini btn-warning" id="ace-settings-btn">
+                            <i class="icon-cog"></i>
+                        </div>
+                        <div id="ace-settings-box">
+                            <div>
+                                <div class="pull-left">
+                                    <select id="skin-colorpicker" class="hidden">
+                                        <option data-class="default" value="#438EB9">#438EB9</option>
+                                        <option data-class="skin-1" value="#222A2D">#222A2D</option>
+                                        <option data-class="skin-2" value="#C6487E">#C6487E</option>
+                                        <option data-class="skin-3" value="#D0D0D0">#D0D0D0</option>
+                                    </select>
+                                </div>
+                                <span>&nbsp; Choose Skin</span>
+                            </div>
+                            <div><input type="checkbox" class="ace-checkbox-2" id="ace-settings-header" /><label class="lbl" for="ace-settings-header"> Fixed Header</label></div>
+                            <div><input type="checkbox" class="ace-checkbox-2" id="ace-settings-sidebar" /><label class="lbl" for="ace-settings-sidebar"> Fixed Sidebar</label></div>
+                        </div>
+                    </div><!--/#ace-settings-container-->
+
+
+            </div><!-- #main-content -->
+        </div><!--/.fluid-container#main-container-->
+
+        <a href="#" id="btn-scroll-up" class="btn btn-small btn-inverse">
+            <i class="icon-double-angle-up icon-only"></i>
+        </a>
+        
+        <?php include"_script.php"?>
 	</body>
 </html>
